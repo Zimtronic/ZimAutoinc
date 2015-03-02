c2fe416cdbd4a67cc5b64875b2b78659d34c3a83
Rene Lopez <rene.lopez@zerofractal.com>
2015-03-01 23:48:56 -0500

    Initial commit

---
+++ Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Zim\AutoincBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('ZimAutoincBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ ZimAutoincBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Zim\AutoincBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ZimAutoincBundle extends Bundle
+{
+}
